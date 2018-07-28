2a07ecc0b0ed8a55b035597efe1c32302eb7a99b
hoangvan1254 <phamthanh121384@gmail.com>
2018-07-28 15:48:47 +0700

    Add Test delete /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -96,3 +96,40 @@ describe('GET /todos/:id', () => {
         .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[0]._id.toHexString();
+        request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).toBe(hexId);
+        })
+        .end((err, res) => {
+            if(err) {
+                return done(err);
+            }
+
+            Todo.findById(hexId).then((todo) => {
+                expect(todo).toNotExist();
+                done();
+            }).catch((e) => done(e));
+        });
+    });
+
+    it('should return a 404 if todo not found', (done) => {
+        var id3 = new ObjectID().toHexString();
+        request(app)
+        .delete(`/todos/${id3}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return a 404 for invalid object id', (done) => {
+        request(app)
+        .delete(`/todos/123abc`)
+        .expect(404)
+        .end(done);
+    });
+});
